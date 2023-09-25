00ddcd8c9bacc217245a31e57870a4069e02ae56
chohee98 <seaotter0320@gmail.com>
2023-09-25 13:05:44 +0900

    Function

--- 3_Function/main.cpp
+++ 3_Function/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//------------------------------------------Calculator
 //함수형
 //unsigned int : 반환 자료형
 //CalculatorSum : 함수 이름
@@ -22,9 +23,9 @@ int CalculatorMultiply(int x, int y)
 	return x * y;
 }
 
-double CalculatorDivide(double x, double y)
+double CalculatorDivide(int x, int y)
 {
-	return x / y;
+	return (double)x / y;
 }
 
 int CalculatorRemain(int x, int y)
@@ -32,16 +33,94 @@ int CalculatorRemain(int x, int y)
 	return x % y;
 }
 
+//------------------------------------------Factorial
+
+int FactorialDown(int Num)
+{
+	int FacResult = 1;
+	for (int i = Num; i > 0; --i)
+	{
+		FacResult *= i;
+	}
+
+	return FacResult;
+}
+
+int FactorialUp(int Num)
+{
+	int FacResult = 1;
+	for (int i = 1; i <= Num; ++i)
+	{
+		FacResult *= i;
+	}
+
+	return FacResult;
+}
+
+int ReFactorial(int Num) //재귀함수
+{
+	if (Num == 1)
+	{
+		return 1;
+	}
+
+	return Num * ReFactorial(Num - 1);
+}
+
+int SumTo(int Num)
+{
+	int Sum = 0;
+	for (int i = 1; i <= Num; ++i)
+	{
+		Sum += i;
+	}
+	return Sum;
+}
+
 int main()
 {
 	//unsigned int nResult = 0;
 	//nResult = CalculatorSum(1, 1);
 	//printf("함수 실행 결과 = %d\n", nResult);
-	int x = 10, y = 3;
+	/*int x = 10, y = 3;
 	printf("x+y=%d\n", CalculatorSum(x, y));
 	printf("x-y=%d\n", CalculatorMinus(x, y));
 	printf("x*y=%d\n", CalculatorMultiply(x, y));
 	printf("x/y=%lf\n", CalculatorDivide(x, y));
-	printf("x%%y=%d\n", CalculatorRemain(x, y));
+	printf("x%%y=%d\n", CalculatorRemain(x, y));*/
+
+	//반복문 for문
+	//for (/*초기 설정*/int i = 0;/*반복 조건 체크*/i < 10;/*반복 후 처리*/i += 2)
+	//{
+	//	//반복 처리할 코드 시작
+	//	printf("%d\n", i);
+	//}
+
+	////Factorial 5!
+	//int FacResult = 1;
+	//int num = 3;
+	//for (int i = 1; i <= num; ++i)
+	//{
+	//	FacResult *= i;
+	//}
+	//printf("%d! = %d\n", num, FacResult);
+	//FacResult = 1;
+	//int FacNum = 4;
+	//for (int i = FacNum; i > 0; --i)
+	//{
+	//	FacResult *= i;
+	//}
+	//printf("%d! = %d\n", FacNum, FacResult);
+
+	////Factorial 함수 호출
+	//int num = 3;
+	//printf("%d! = %d\n", num, FactorialUp(num));
+
+	//1~n까지 합 구하는 함수
+	//int SumTo(int Num)
+	int num = 5;
+	printf("1~%d까지의 합 = %d\n", num, SumTo(num));
+
+
 	return 0;
 }
