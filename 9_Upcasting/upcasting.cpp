4dbc6b7c7f56696e431db19df4cb825243ce88cc
cho <seaotter0320@gmail.com>
2023-10-05 14:51:51 +0900

    upCasting

--- 9_Upcasting/upcasting.cpp
+++ 9_Upcasting/upcasting.cpp
@@ -14,39 +14,89 @@ class CBase
 {
 public:
 	int m_X;
-
-	CBase(int x) :m_X(0) {
+	int m_Y;
+public:
+	CBase(int x = 1, int y = 2) :m_X(x), m_Y(y) {
 		printf("부모 클래스(CBase) 생성\n");
 	}
 
 	~CBase() {
 		printf("부모 클래스(CBase) 파괴\n");
 	}
+
+	/*
+	//가상 함수(virtual function)
+	//자식 클래스에서 재정의할 것으로 기대하는 멤버함수
+	//함수 앞에 virtual 키워드를 붙인다.
+	virtual void printValue()
+	{
+		printf("m_X=%d\n", m_X);
+	}
+	*/
 };
 
 //상속 방법: ": public CBase"와 같이 클래스 뒤에 상속할 부모 클래스를 선언
 class CChild : public CBase
 {
 public:
-	int m_Y;
-
+	int m_Z;
+public:
 	//":" 뒤에 부모클래스(CBase)를 넣어서 부모클래스의 생성자 호출
 	//부모 클래스의 생성자가 먼저 호출 후 자식 생성자가 호출됨
-	CChild(int x, int y) :m_Y(y), CBase(x) {
+	CChild(int x = 1, int y = 2, int z = 3) :m_Z(z), CBase(x, y) {
 		printf("자식 클래스(CChild) 생성\n");
 	}
+
 	//자식 파괴자 먼저 호출 후 부모 파괴자 호출됨
 	~CChild() {
 		printf("자식 클래스(CChild) 파괴\n");
 	}
+
+	/*
+	virtual void printValue() override
+	{
+		CBase::printValue(); //부모에서의 처리 호출 - 부모의 클래스에서 스코프 연산으로 함수 호출
+		printf("m_X=%d, m_Z=%d\n", m_X, m_Z);
+	}
+	*/
 };
+
+//Upcasting(업케스팅)
+//자식 -> 부모로 별다른 연산자 없이 자동으로 케스팅(읽어지는) 되는 방법
+void printObject(CBase** pBase, int n)	//CBase 사이즈가 (2+2)byte이기 때문에 전체 배열을 2개씩 끊어서 출력 (업케스팅 문제)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("xpos=%d, ypos%d\n", pBase[i]->m_X, pBase[i]->m_Y);
+	}
+}
 
 int main()
 {
+	/*
 	CBase base(10);
 	base.m_X = 100;
+	base.printValue();
 
 	CChild child(10, 20);
 	child.m_X = 200;
+	child.printValue();	//자식 클래스에서 재정의한 함수 호출
+	*/
+	/*
+	CChild* pArrChild = new CChild[10];
+	printObject(pArrChild, 10);
+	delete[] pArrChild;
+	*/
 
+	CBase** pArrBase = new CBase * [10];
+	for (int i = 0; i < 10; i++)
+	{
+		pArrBase[i] = new CChild;
+	}
+	printObject(pArrBase, 10);
+	for (int i = 0; i < 10; i++)
+	{
+		delete pArrBase[i];
+	}
+	delete[] pArrBase;
 }
